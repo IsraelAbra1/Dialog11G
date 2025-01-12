ce6673179db4e40ab7176957d64a27b163105d18
IsraelAbra1 <Israel.kugel10@gmail.com>
2025-01-12 08:40:15 +0200

    First Version

--- app/src/main/java/com/example/dialogwxprojb/CustomDialog.java
+++ app/src/main/java/com/example/dialogwxprojb/CustomDialog.java
@@ -16,6 +16,7 @@ public class CustomDialog extends Dialog implements View.OnClickListener
         super(context);
         setContentView(R.layout.custom_dialog);
 
+
         this.context = context;
 
         this.btnYes = findViewById(R.id.btnYes);
